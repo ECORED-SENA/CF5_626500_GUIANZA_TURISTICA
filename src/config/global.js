5c5ba2443fde31fd70e0a532bc1c193a521da8ba
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-09-13 10:31:38 -0500

    tema1 listo

--- src/config/global.js
+++ src/config/global.js
@@ -1,18 +1,19 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
+    componenteFormativo: 'Caracterización del destino ',
+    descripcionCurso:
+      'Con el estudio de este componente, el aprendiz estará en capacidad de aplicar saberes y habilidades relacionados con la caracterización del destino, para la guianza turística. También, Afianzará su capacidad para describir, interpretar, narrar situaciones e historias que, en el marco del servicio guiado, ayudarán a que el cliente experimente y viva lo que se cuenta,  satisfaciendo su expectativa e interés.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
-      {
-        clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
-      },
-      {
-        clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
-      },
+      // {
+      //   clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
+      //   imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+      // },
+      // {
+      //   clases: ['banner-principal-decorativo-2'],
+      //   imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      // },
     ],
   },
   menuPrincipal: {
@@ -32,30 +33,43 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Técnicas de redacción y escritura.',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Género narrativo creativo',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema3',
         icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Texto narrativo creativo',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema4',
+        icono: 'far fa-file-alt',
+        numero: '4',
+        titulo: 'Técnicas de redacción creativa',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema5',
+        icono: 'far fa-file-alt',
+        numero: '5',
+        titulo: 'Escritura creativa',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema6',
+        icono: 'far fa-file-alt',
+        numero: '6',
+        titulo: 'Derechos de autor',
         desarrolloContenidos: true,
       },
     ],
@@ -101,32 +115,105 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: '3. Texto narrativo creativo',
+      referencia:
+        'Grapsas, T. (2020) Marketing de contenidos para el turismo, ¿Ya sabes cómo aplicar la estrategia para el sector?',
+      tipo: 'Blog',
+      link:
+        'https://rockcontent.com/es/blog/marketing-de-contenidos-para-el-turismo/',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: '4.Técnicas de redacción creativa',
+      referencia:
+        'RAE (2010) Novedades de la Ortografía de la lengua española ',
+      tipo: 'Guía',
+      link:
+        'https://www.rae.es/sites/default/files/Principales_novedades_de_la_Ortografia_de_la_lengua_espanola.pdf',
+    },
+    {
+      tema: '4.Técnicas de redacción creativa',
+      referencia: 'RAE (s.f.) Ortografía Esencial',
+      tipo: 'Guía',
+      link: 'https://www.rae.es/sites/default/files/Ortografia_Esencial_0.pdf',
+    },
+    {
+      tema: '5. Escritura creativa',
+      referencia:
+        'Álvarez, M. (2008) Escritura creativa. Aplicación de las técnicas de Gianni Rodari',
+      tipo: 'Artículo',
+      descarga: 'downloads/doc1.pdf',
+    },
+    {
+      tema: '7. Derechos de autor',
+      referencia: 'Jaramillo, A. (2010) Manual de derecho de autor',
+      tipo: 'Manual',
+      link:
+        'http://derechodeautor.gov.co:8080/documents/10181/331998/Cartilla+derecho+de+autor+%28Alfredo+Vega%29.pdf/e99b0ea4-5c06-4529-ae7a-152616083d40',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Género',
+      significado:
+        'Centrados en su significado en el contexto de las artes: se llama género a cada una de las clases o categorías en que pueden clasificarse las obras de acuerdo con sus características.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Didáctica ',
+      significado:
+        'La didáctica, es el área de la pedagogía vinculada a los métodos prácticos de enseñanza.',
+    },
+    {
+      termino: 'Ortografía',
+      significado:
+        'Es el conjunto de normas que regulan la escritura. Forma parte de la gramática normativa ya que establece las reglas para el uso correcto de las letras y los signos de puntuación.',
+    },
+    {
+      termino: 'Gramática',
+      significado:
+        'Es la ciencia que tiene como objeto de estudio a los componentes de una lengua y sus combinaciones. El concepto halla su origen en el término en latín grammatĭca y hace referencia, por otra parte, al arte de dominar una lengua de modo correcto, tanto desde el habla como con la escritura.',
+    },
+    {
+      termino: 'Sintaxis',
+      significado:
+        'Proviene del término en latín syntaxis, que a su vez deriva de un vocablo griego que se traduce al español como “coordinar”. Se trata de la rama de la gramática que ofrece pautas creadas para saber cómo unir y relacionar palabras a fin de elaborar oraciones y expresar conceptos de modo coherente.',
+    },
+    {
+      termino: 'Derechos de autor',
+      significado:
+        'El derecho de autor está compuesto por normas que protegen los derechos patrimoniales y morales del individuo. Estos derechos incluso se encuentran contemplados en la Declaración Universal de los Derechos Humanos.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Álvarez, M. (2008) Escritura creativa. Aplicación de las técnicas de Gianni Rodari.',
+      link: 'https://www.redalyc.org/pdf/356/35614571010.pdf',
+    },
+    {
+      referencia:
+        'Legorburu, G. y McColl, D. (2014) STORYSCAPING. New York Times Bestseller.',
       link: '',
+    },
+    {
+      referencia:
+        'Mero, C. (2011) Empleo de las Tics en la Aplicación de las Reglas Ortográficas en la Escritura de los estudiantes de Educación Básica. ',
+      link:
+        'http://repositorio.uisrael.edu.ec/bitstream/47000/123/1/UISRAEL-EC-SIS-378.242-175.pdf',
+    },
+    {
+      referencia:
+        'RAE (2010) Novedades de la Ortografía de la lengua española. ',
+      link:
+        'https://www.rae.es/sites/default/files/Principales_novedades_de_la_Ortografia_de_la_lengua_espanola.pdf',
+    },
+    {
+      referencia: 'RAE (s.f.) Ortografía Esencial. ',
+      link: 'https://www.rae.es/sites/default/files/Ortografia_Esencial_0.pdf',
+    },
+    {
+      referencia: 'Rosado, J. (2021) Componentes de la narrativa. ',
+      link:
+        'https://idoc.pub/documents/componentes-de-la-narrativa-34m7xrmywp46',
     },
   ],
   creditos: {
@@ -144,6 +231,26 @@ export default {
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
+      {
+        nombre: 'Tulio Pizarro',
+        cargo: 'Experto temático',
+        centro: 'Regional Distrito Capital - Centro Nacional de Hotelería',
+      },
+      {
+        nombre: 'Zvi Daniel Grosman',
+        cargo: 'Diseñador Instruccional',
+        centro: 'Centro de Gestión Industrial - Regional Distrito Capital ',
+      },
+      {
+        nombre: 'Carolina Coca Salazar',
+        cargo: 'Asesor Metodológico',
+        centro: 'Centro Diseño y Metrología - Regional Distrito Capital ',
+      },
+      {
+        nombre: 'Sandra Patricia Hoyos Sepúlveda',
+        cargo: 'Corrección de estilo',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital ',
+      },
     ],
     desarrolloProducto: [
       {
@@ -153,25 +260,25 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Fabian Zarate',
         cargo: 'Diseño web',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: '',
         cargo: 'Producción audiovisual',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Camilo Andres Bolaño Rey',
         cargo: 'Desarrollo front-end',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Melina Tatiana Quintero Rodriguez',
         cargo: 'Validación de diseño y contenido',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,224 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
-      
+      h1 Descripción del destino
+    .row.justify-content-center.mb-5(data-aos='zoom-in-up')
+      .col-lg-5: img(src='@/assets/curso/temas/tema1/1.jpg', alt='')
+      .col-lg-7 
+        p.mb-3(data-aos='fade-right') Un destino turístico, es una parte del territorio, que cuenta con una cantidad de atractivos turísticos de gran belleza, que generan interés de ser visitados por turistas de diferentes lugares y por razones diversas, como: recreación, uso del tiempo libre, desarrollo de actividades físicas, culturales o sociales, que satisfagan las necesidades de los turistas.
+          br
+          br
+          | El destino turístico, no está determinado únicamente por el estado de sus atractivos, sino por sus valores asociados, tales como:
+        ul.lista-ul--color
+          li.mb-0
+            i.vineta
+            | El talento humano.
+          li.mb-0
+            i.vineta
+            | Las empresas prestadoras de servicios turísticos.
+          li.mb-0
+            i.vineta
+            | Los hoteles, restaurantes, bares, museos, entre otros.
+          li.mb-0
+            i.vineta
+            | Los servicios de soporte a la actividad turística.
+          li.mb-0
+            i.vineta
+            | Servicios públicos, infraestructura, accesibilidad.
+    .row.justify-content-center.my-5
+      .col-lg-10
+        .tarjeta-importante.position-relative.p-4(style='background-color: #ffe6a1')(data-aos='zoom-in-up')
+          .img-importante.d-none.d-lg-block: img(src='@/assets/curso/temas/tema1/2.png', alt='' style='width: 399px')
+          
+          .row.justify-content-center.align-items-center
+            .col-lg-3
+              div(style='widht: 399px; height: 150px').d-none.d-lg-block
+            .col-lg-6
+              h5 ¡Importante!
+              p Todo lo anterior se conjuga para que el destino se constituya en un “destino turístico” competitivo y sostenible.
+            .col-auto: img(src='@/assets/curso/temas/tema1/3.svg', alt='')
+    Separador
+    #t_1_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 1.1 Destino turístico: concepto y elementos
+    .row.justify-content-center.align-items-center.mb-5
+      .col-auto: img(src='@/assets/curso/temas/tema1/4.svg', alt='')
+      .col-lg-10
+        .bloque-texto-d.color-gris.p-4
+          .bloque-texto-d__texto.mb-2
+            i.fas.fa-quote-left
+
+            h4.text-regular Según la Organización Mundial del Turismo, (2007) 
+            p  “un destino turístico local es un espacio físico en el que un visitante pasa, al menos, una noche. Incluye productos turísticos, tales como servicios de apoyo, atractivos y recursos turísticos a una distancia, que permite ir y volver en un día…”.
+            i.fas.fa-quote-right
+    .row.justify-content-center.mb-5
+      .col-lg-6
+        .bloque-texto-c.color-primario.p-4(style='background-color: #ffeccd')
+          i.fas.fa-quote-right
+          h4.mb-2.fw-normal “…Tiene unos límites físicos y administrativos que definen su gestión, imágenes y percepciones que determinan su competitividad en el mercado. Los destinos locales incorporan a diversos grupos, entre los que se cuenta a menudo la comunidad anfitriona, y pueden establecer lazos y redes entre sí para construir destinos mayores”.
+          span (Organización Mundial del Turismo, 2007)
+      .col-lg-6: img(src='@/assets/curso/temas/tema1/5.jpg', alt='')
+    .bg-1.margin-n.p-5(data-aos='zoom-in-up')
+      .tarjeta.bg-white.box-shadow.p-5.mb-5
+        p.mb-5(data-aos='fade-right') Para considerarse como tal, los elementos con los que debe contar un destino turístico son:
+        .row.justify-content-center.mb-5
+          .col-lg-10
+            .bg-arboles.mb-4
+              figure
+                img(src='@/assets/curso/temas/tema1/6.svg', alt='Texto que describa la imagen')
+        p.mb-5(data-aos='fade-right') Analice los elementos que configuran a un sitio como Destino Turístico, los cuales se enuncian a continuación:
+        .tarjeta.bg-slyder.p-5.mb-5(data-aos='zoom-in-up')
+          SlyderA(tipo='b')
+            .row
+              .col-md-5.mb-4.mb-md-0
+                h4 Atractivos turísticos
+                p Considerados la materia prima del turismo. Algunas investigaciones afirman que, para la presentación de proyectos, los atractivos turísticos son: los lugares capaces de generar desplazamientos turísticos y son el componente principal del producto turístico.
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema1/7.jpg', alt='Texto que describa la imagen')
+            .row
+              .col-md-5.mb-4.mb-md-0
+                h4 La infraestructura
+                p Conjunto de todos los servicios, que complementan la Planta Turística y dentro de los cuales se pueden mencionar: el transporte, seguridad, sanidad, condiciones de acceso, servicios públicos, entre otros.
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema1/8.jpg', alt='Texto que describa la imagen')
+            .row
+              .col-md-5.mb-4.mb-md-0
+                h4 Servicios turísticos de apoyo
+                p Permiten atender y satisfacer las necesidades de los turistas. De ellos surgen los núcleos receptores constituidos por el equipamiento e instalaciones (alojamientos, alimentación, sitios de esparcimiento, agencias de viajes, traslados, rentas de autos, circuitos turísticos, excursiones, información, guías turísticos), que demandan los turistas.
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema1/9.jpg', alt='Texto que describa la imagen')
+            .row
+              .col-md-5.mb-4.mb-md-0
+                h4 Capital humano del sector turístico
+                p Son las personas capacitadas y formadas para hacer de la actividad turística un recurso productivo y de utilidad. Tienen habilidades y competencias que hacen sentir bienvenidos a los turistas y visitantes. Hacen parte de la cadena de valor del turismo.
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema1/10.jpg', alt='Texto que describa la imagen')
+            .row
+              .col-md-5.mb-4.mb-md-0
+                h4 Planificación turística del territorio
+                p Se basa en los Planes de Desarrollo Territorial (POT), que reflejan lo que la comunidad dentro de ese territorio desea y necesita. Su objetivo principal es direccionar de manera estratégica los procesos turísticos de un territorio.  
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema1/11.jpg', alt='Texto que describa la imagen')
+            .row
+              .col-md-5.mb-4.mb-md-0
+                h4 La gestión de los destinos
+                p Se vincula directamente con la planificación, debido a que una vez que se determina por parte de la comunidad, si el territorio tiene las potencialidades turísticas necesarias para ser un destino turístico, se desarrolla toda una gestión que involucra: Investigación de mercado, diseño de producto, comercialización, prestación de servicios de turismo, promoción de la inversión.
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema1/12.jpg', alt='Texto que describa la imagen')
+        p.mb-5(data-aos='fade-right') La gestión de los destinos implica el desarrollo de toda una gestión, que involucra procesos como:
+        .row.justify-content-center.mb-5
+          .col-lg-10
+            img(data-aos='zoom-in-up')(src='@/assets/curso/temas/tema1/13.svg', alt='')
+        .row.justify-content-center.my-5
+          .col-lg-11
+            .tarjeta-importante.position-relative.p-4(style='background-color: #ffe6a1')(data-aos='zoom-in-up').mb-5
+              .img-importante.d-none.d-lg-block: img(src='@/assets/curso/temas/tema1/2.png', alt='' style='width: 399px')
+              
+              .row.justify-content-center.align-items-center
+                .col-lg-3
+                  div(style='widht: 399px; height: 150px').d-none.d-lg-block
+                .col-lg-6
+                  h5 ¡Importante!
+                  p Un destino turístico está siempre condicionado por un entorno geográfico, al igual que factores políticos, administrativos, económicos, culturales, ambientales y sociales.
+                .col-auto: img(src='@/assets/curso/temas/tema1/14.svg', alt='')
+    Separador.my-5
+    #t_1_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 1.2  Sistema turístico
+    div(style='background-color: #d9edf8')
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-6
+          .p-5 
+            p El turismo es un fenómeno humano que debe ser visto desde una visión sistémica, dada la amplia variedad de impactos y alcances que genera la actividad turística y los actores que participan en su cadena de valor. Así, para la comprensión del amplio sistema turístico, se ha de dar relevancia e incluir a todos los ámbitos en los que se desarrolla el turismo, integrando los ámbitos social, cultural, económico, político, institucional, privado, público y educativo.
+        .col-6: img(src='@/assets/curso/temas/tema1/15.png', alt='')
+    p.mb-5(data-aos='fade-right') El siguiente recurso le muestra algunas conceptualizaciones, que han aportado a la teoría de los sistemas y su aplicación al turismo:
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    p.mb-5(data-aos='fade-right') La siguiente tabla le ampliará algunos conceptos del sistema turístico. Asimile cada concepto y procure llevar registro de lo más importante en su libreta personal de apuntes:
+    .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+      h5 Tabla 1
+      span Conceptos del Sistema Turístico
+    .tabla-b.color-acento-contenido.mb-5
+      table
+        caption Leyenda de la tabla
+        tr(style='border-top: 5px solid #eb6f2d')
+          th(style='background-color: #e8e8e8') 
+            h4.mb-0 CONCEPTO
+          td
+            h5.mb-0 DEFINICIÓN
+        tr
+          th(style='background-color: #e8e8e8').text-center  Oferta y demanda
+          td La demanda turística es el número de personas que viajan o desean viajar para utilizar instalaciones turísticas y servicios lejos de sus lugares de trabajo o residencia.
+            br
+            br
+            |La oferta turística está compuesta por bienes y servicios que ofrece un destino y generan atracción hacia los turistas, logrando satisfacer sus necesidades y expectativas.
+        tr
+          th(style='background-color: #e8e8e8').text-center  Atractivos
+          td Todos aquellos bienes y servicios que por intermedio de la actividad del hombre y de los medios con que cuenta, hacen posible la actividad turística y satisfacen las actividades de la demanda. Un recurso turístico constituye la existencia de un conjunto potencial (conocido o desconocido) de los bienes materiales o inmateriales a disposición del hombre y que pueden utilizarse mediante un proceso de transformación que hacen posible la actividad turística y satisfacen las necesidades de la demanda.
+        tr
+          th(style='background-color: #e8e8e8').text-center Comunidad receptora
+          td Grupo de personas de una localidad que recibe a los turistas.
+        tr
+          th(style='background-color: #e8e8e8').text-center  Servicios Turísticos
+          td Es una base económica viable para edificar y sostener una empresa en el sector, siempre que dicho producto o servicio encuentre mercados y sea fuente de rentabilidad a un determinado nivel de precios.
+        tr
+          th(style='background-color: #e8e8e8').text-center Infraestructura 
+          td Conjunto de obras y servicios básicos que son de gran importancia para el funcionamiento del turismo de un país.
+        tr
+          th(style='background-color: #e8e8e8').text-center Superestructura 
+          td Organismos públicos y privados que, de manera directa o indirecta, coordinan todo lo necesario para que el turismo se dé con éxito.
+    Separador
+    #t_1_3.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 1.3  Potencial turístico
+    .bg-arboles.p-3
+      .row.justify-content-center.mb-5
+        .col-lg-7
+          p La valoración de los recursos, en los destinos, busca su eventual ampliación y desarrollo turístico, así como determinar las actividades turísticas a ofertar. Para ello, es pertinente evaluar minuciosamente y elaborar un inventario turístico con la información más relevante de esos recursos. Estas son las razones por las cuales es importante identificar el potencial de un destino turístico.
+            br
+            br
+            |Tenga en cuenta los siguientes aspectos del proceso de identificación del potencial de un destino turístico:
+          ul.lista-ul--color
+            li
+              i.vineta
+              | Para elaborar el inventario turístico, se identifican los atractivos y actividades turísticas vigentes o futuras de los recursos naturales.
+            li
+              i.vineta
+              | Para identificar el potencial de un destino turístico se tienen en cuenta varios factores fundamentales como:  
+              li.ms-5
+                .lista-ol--cuadro__vineta
+                  span 1
+                | El patrimonio cultural (material e inmaterial)
+
+              li.ms-5
+                .lista-ol--cuadro__vineta
+                  span 2
+                | Las características del territorio, la economía local, los centros urbanos y zonas rurales.
+
+              li.ms-5
+                .lista-ol--cuadro__vineta
+                  span 3
+                | Los servicios de toda índole (turísticos, de salud, seguridad), los sitios de culto, infraestructura, el recurso humano.
+              li.ms-5
+                .lista-ol--cuadro__vineta
+                  span 4
+                | El análisis del mercado y de la competencia.
+              li.ms-5
+                .lista-ol--cuadro__vineta
+                  span 5
+                | La voluntad política y la percepción de las comunidades.
+            li
+              i.vineta
+              | En suma, el potencial de un destino turístico está determinado por la oferta de servicios locales y la demanda, que suple los requerimientos de visitantes y turistas.
+        .col-lg-5: img(src='@/assets/curso/temas/tema1/16.png', alt='')
+
+
+
+
 </template>
 
 <script>
@@ -27,4 +243,28 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.bloque-texto-d.color-gris
+  border-top: 1px solid #406834
+  &:before
+    background-color: #e2e8e0
+.margin-n
+  margin: -3rem
+.bg-1
+  background-image: url(../assets/curso/temas/tema1/bg-1.svg)
+  background-repeat: no-repeat
+  background-position: start
+  background-size: cover
+.bg-arboles
+  background-image: url(../assets/curso/temas/tema1/bg-arboles.svg)
+  background-repeat: no-repeat
+  background-position: start
+  background-size: cover
+.bg-slyder
+  background-image: url(../assets/curso/temas/tema1/bg-slyder.svg)
+  background-repeat: no-repeat
+  background-position: 100% 100%
+  background-size: cover
+.box-shadow
+  box-shadow: 4px 14px 30px -15px rgba(0,0,0,0.59)
+</style>
